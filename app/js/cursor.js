6ac9b6e060b272ceaecf68edc3769948fa66d0bd
Parazeeknova <yesh8harsh+github@gmail.com>
2024-04-25 08:48:11 +0530

    Optimized using Event delegation
    cursor.js caused lag on certain devices

--- app/js/cursor.js
+++ app/js/cursor.js
@@ -1,40 +1,30 @@
-const cursor = document.querySelector('.cursor')
-const outline = document.querySelector('.outline')
-const links = document.querySelectorAll('a')
-const buttons = document.querySelectorAll('button')
+const cursor = document.querySelector('.cursor');
+const outline = document.querySelector('.outline');
 
 document.addEventListener('mousemove', function (e) {
-  cursor.setAttribute('style', 'top: ' + (e.pageY - 5) + 'px; left: ' + (e.pageX - 5) + 'px;')
-  outline.setAttribute('style', 'top: ' + (e.pageY - 40) + 'px; left: ' + (e.pageX - 40) + 'px;')
-})
+  cursor.setAttribute('style', 'top: ' + (e.pageY - 5) + 'px; left: ' + (e.pageX - 5) + 'px;');
+  outline.setAttribute('style', 'top: ' + (e.pageY - 40) + 'px; left: ' + (e.pageX - 40) + 'px;');
+});
 
 document.addEventListener('click', function () {
-  cursor.classList.add('cursor--clicked')
-  outline.classList.add('outline--hidden')
+  cursor.classList.add('cursor--clicked');
+  outline.classList.add('outline--hidden');
   setTimeout(function () {
-    cursor.classList.remove('cursor--clicked')
-    outline.classList.remove('outline--hidden')
-  }, 400)
-})
+    cursor.classList.remove('cursor--clicked');
+    outline.classList.remove('outline--hidden');
+  }, 400);
+});
 
-links.forEach(function (link) {
-  link.addEventListener('mouseover', function () {
-    cursor.classList.add('cursor--link-hovered')
-    outline.classList.add('outline--hidden')
-  })
-  link.addEventListener('mouseout', function () {
-    cursor.classList.remove('cursor--link-hovered')
-    outline.classList.remove('outline--hidden')
-  })
-})
+document.addEventListener('mouseover', function (event) {
+  if (event.target.matches('a, button')) {
+    cursor.classList.add('cursor--link-hovered');
+    outline.classList.add('outline--hidden');
+  }
+});
 
-buttons.forEach(function (button) {
-  button.addEventListener('mouseover', function () {
-    cursor.classList.add('cursor--link-hovered')
-    outline.classList.add('outline--hidden')
-  })
-  button.addEventListener('mouseout', function () {
-    cursor.classList.remove('cursor--link-hovered')
-    outline.classList.remove('outline--hidden')
-  })
-})
+document.addEventListener('mouseout', function (event) {
+  if (event.target.matches('a, button')) {
+    cursor.classList.remove('cursor--link-hovered');
+    outline.classList.remove('outline--hidden');
+  }
+});
